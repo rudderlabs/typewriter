a4238459bd443b66b2999a8fcedaff76906b3f6c
Desu Sai Venkat <saivenkatdesu@gmail.com>
2022-03-14 16:27:48 +0530

    Initial Changes for TypeScript Support for RudderTyper

--- src/generators/javascript/javascript.ts
+++ src/generators/javascript/javascript.ts
@@ -136,10 +136,20 @@ export const javascript: Generator<
 	generateRoot: async (client, context) => {
 		// index.hbs contains all JavaScript client logic.
 		await client.generateFile<JavaScriptRootContext>(
-			'index.js',
+			client.options.client.language === Language.TYPESCRIPT ? 'index.ts' : 'index.js',
 			'generators/javascript/templates/index.hbs',
 			context
 		)
+
+		// rudder.hbs contains the TypeScript definitions for the Rudder API.
+		// It becomes an empty file for JavaScript after being transpiled.
+		if (client.options.client.language === Language.TYPESCRIPT) {
+			await client.generateFile<JavaScriptRootContext>(
+				'rudder.ts',
+				'generators/javascript/templates/rudder.hbs',
+				context
+			)
+		}
 	},
 	formatFile: (client, file) => {
 		let { contents } = file
@@ -160,7 +170,7 @@ export const javascript: Generator<
 
 		// Apply stylistic formatting, via Prettier.
 		const formattedContents = prettier.format(contents, {
-			parser: 'babel',
+			parser: client.options.client.language === Language.TYPESCRIPT ? 'typescript' : 'babel',
 			// Overwrite a few of the standard prettier settings to match with our RudderTyper configuration:
 			useTabs: true,
 			singleQuote: true,
