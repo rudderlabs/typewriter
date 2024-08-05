84765781fea5db31ac3b3ad4be23c7a706f33c92
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-08-05 14:36:12 +0530

    chore: provide setup to initialise rudder sdk

--- tests/e2e/android-kotlin/app/src/main/java/com/rudderstack/typewriterexample/MainApplication.kt
+++ tests/e2e/android-kotlin/app/src/main/java/com/rudderstack/typewriterexample/MainApplication.kt
@@ -1,10 +1,22 @@
 package com.rudderstack.typewriterexample
 
 import android.app.Application
+import com.rudderstack.android.sdk.core.RudderClient
+import com.rudderstack.android.sdk.core.RudderConfig
+import com.rudderstack.android.sdk.core.RudderLogger
 
 class MainApplication: Application() {
     override fun onCreate() {
         super.onCreate()
-        
+
+        // Initialize RudderStack SDK
+        RudderClient.getInstance(
+            this,
+            "<WRITE_KEY>",
+            RudderConfig.Builder()
+                .withDataPlaneUrl("<DATA_PLANE_URL>")
+                .withLogLevel(RudderLogger.RudderLogLevel.VERBOSE)
+                .build()
+        )
     }
 }
