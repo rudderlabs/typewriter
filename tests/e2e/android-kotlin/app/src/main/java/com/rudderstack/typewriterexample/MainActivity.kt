98a31598be480422b93a898fcb7fb615cc846a9d
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-08-05 15:23:15 +0530

    feat: add setup to make track events using rudder android sdk

--- tests/e2e/android-kotlin/app/src/main/java/com/rudderstack/typewriterexample/MainActivity.kt
+++ tests/e2e/android-kotlin/app/src/main/java/com/rudderstack/typewriterexample/MainActivity.kt
@@ -4,26 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.rudderstack.android.sdk.core.RudderClient
 import com.rudderstack.typewriterexample.ui.theme.TypeWriterExampleTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TypeWriterExampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MakeEvents(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +34,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+private fun MakeEvents(modifier: Modifier = Modifier) {
+    Row(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .wrapContentHeight(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Button(
+            onClick = {
+                makeEvents()
+            }
+        ) {
+            Text(text = "Make RudderTyper Events")
+        }
+    }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TypeWriterExampleTheme {
-        Greeting("Android")
-    }
+private fun makeEvents() {
+    // Make RudderTyper events here
+    val analytics: RudderClient? = RudderClient.getInstance()
+    analytics?.track("New track Event")
 }
