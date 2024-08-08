3780d0ab4588e34bfc39ea8e5a530f5ac2cbaae9
Satheesh Kannan <satheesh@rudderstack.com>
2024-08-08 22:29:40 +0530

    chore: add new sample iOS - ObjectiveC app

---
+++ tests/e2e/ios-objc/RudderTyperExample/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  RudderTyperExample
+//
+//  Created by Satheesh Kannan on 08/08/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tests/e2e/ios-objc/RudderTyperExample/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  RudderTyperExample
+//
+//  Created by Satheesh Kannan on 08/08/24.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
