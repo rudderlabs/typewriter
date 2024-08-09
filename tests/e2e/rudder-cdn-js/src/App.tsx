3d76696797820dc0b5fe16d9272a1de9b76044a8
MoumitaM <mandal.moumita2013@gmail.com>
2024-08-09 12:25:03 +0530

    chore: sample apps for rudder typer

---
+++ tests/e2e/rudder-cdn-js/src/App.tsx
@@ -0,0 +1,78 @@
+import logo from './logo.svg';
+import './App.css';
+import { RudderTyperAnalytics } from './analytics/index';
+
+function App() {
+  const page = () => {
+    window.rudderanalytics?.page(
+      'Cart',
+      'Cart Viewed',
+      {
+        path: '/best-seller/1',
+        referrer: 'https://www.google.com/search?q=estore+bestseller',
+        search: 'estore bestseller',
+        title: 'The best sellers offered by EStore',
+        url: 'https://www.estore.com/best-seller/1',
+      },
+      () => {
+        console.log('page call');
+      },
+    );
+  };
+  const identify = () => {
+    window.rudderanalytics?.identify(
+      'sample-user-123',
+      {
+        firstName: 'Alex',
+        lastName: 'Keener',
+        email: 'alex@example.com',
+        phone: '+1-202-555-0146',
+      },
+      () => {
+        console.log('identify call');
+      },
+    );
+  };
+  const track = () => {
+    RudderTyperAnalytics.sampleEvent1({
+      'Sample property 1': 'Sample value 1',
+    });
+  };
+  const alias = () => {
+    window.rudderanalytics?.alias('alias-user-id', () => {
+      console.log('alias call');
+    });
+  };
+  const group = () => {
+    window.rudderanalytics?.group(
+      'sample_group_id',
+      {
+        name: 'Apple Inc.',
+        location: 'USA',
+      },
+      () => {
+        console.log('group call');
+      },
+    );
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <div className="card">
+          <button onClick={page}>page</button>
+          <button onClick={identify}>identify</button>
+          <button onClick={track}>track</button>
+          <button onClick={group}>group</button>
+          <button onClick={alias}>alias</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
