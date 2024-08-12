374391a7c9c69425297609c61abcfa2fa6a41f7f
Satheesh Kannan <satheesh@rudderstack.com>
2024-08-13 00:22:38 +0530

    chore: add class comments to model class

--- tests/e2e/ios-swift/RudderTyperSwiftExample/RudderConfig/RudderConfig.swift
+++ tests/e2e/ios-swift/RudderTyperSwiftExample/RudderConfig/RudderConfig.swift
@@ -7,11 +7,26 @@
 
 import UIKit
 
+/**
+ This is a model class for Rudder SDK configuration.
+ */
 struct RudderConfig: Codable {
+    /**
+     * Write key for the Rudder SDK.
+     */
     let writeKey: String
+    /**
+     * Data plane url string for the Rudder SDK.
+     */
     let dataPlaneUrl: String
+    /**
+     * Control plane url string for the Rudder SDK.
+     */
     let controlPlaneUrl: String
     
+    /**
+     * Coding keys to generate the model values.
+     */
     enum CodingKeys: String, CodingKey {
         case writeKey = "WRITE_KEY"
         case dataPlaneUrl = "DATA_PLANE_URL"
