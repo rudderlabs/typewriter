0f4f9919a5bea0606f0072a4a5e335e1ac5d43de
Satheesh Kannan <satheesh@rudderstack.com>
2024-08-12 23:29:08 +0530

    chore: add new sample iOS - Swift app

---
+++ tests/e2e/ios-swift/RudderTyperSwiftExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RudderTyperSwiftExample
+//
+//  Created by Satheesh Kannan on 12/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
